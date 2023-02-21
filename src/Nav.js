4767b89472dcd7c6bba1f2e021d50cd27d5bbf38
xavier <zavytynes@gmail.com>
2023-02-21 12:33:20 -0500

    Test2

--- src/Nav.js
+++ src/Nav.js
@@ -9,3 +9,12 @@ function Nav() {
 }
 
 export default Nav;
+
+<ul>
+    <li>Home</li>
+    <li>About</li>
+    <li>Menu</li>
+    <li>Reservations</li>
+    <li>Order Online</li>
+    <li>Login</li>
+</ul>
